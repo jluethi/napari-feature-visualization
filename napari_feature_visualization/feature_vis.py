2bcf3f76593dd20ff04d44cea010441cb488cc35
jluethi <joel.luethi@uzh.ch>
2021-04-26 17:38:18 +0200

    Add dropdown menus for the feature selection and improve the contrast limit setting

--- napari_feature_visualization/feature_vis.py
+++ napari_feature_visualization/feature_vis.py
@@ -14,39 +14,78 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from enum import Enum
+from functools import lru_cache
+
 
 class ColormapChoices(Enum):
     viridis='viridis'
     test='test'
 
+
+@lru_cache(maxsize=16)
+def get_df(path):
+    return pd.read_csv(path)
+
+
+def _init(widget):
+    @widget.DataFrame.changed.connect
+    def update_df_columns(event):
+        # Implemented following inputs from Talley Lambert:
+        # https://forum.image.sc/t/visualizing-feature-measurements-in-napari-using-colormaps-as-luts/51567/16
+        # event value will be the new path
+        # get_df will give you the cached df
+        df = get_df(event.value)
+        features = list(df.columns)
+        widget.feature.choices = features
+        widget.label_column.choices = features
+        if 'label' in features:
+            widget.label_column.value = 'label'
+        elif 'Label' in features:
+            widget.label_column.value = 'Label'
+        elif 'index' in features:
+            widget.label_column.value = 'index'
+
+
+    @widget.feature.changed.connect
+    def update_rescaling(event):
+        df = get_df(widget.DataFrame.value)
+        # TODO: Check whether feature is in df
+        try:
+            quantiles=(0.01, 0.99)
+            widget.lower_contrast_limit.value = df[event.value].quantile(quantiles[0])
+            widget.upper_contrast_limit.value = df[event.value].quantile(quantiles[1])
+        except KeyError:
+            # Don't update the limits if a feature name is entered that isn't in the dataframe
+            pass
+
+
+
+# TODO: Set better limits for contrast_limits
 @magic_factory(
         call_button="Apply Feature Colormap",
         layout='vertical',
         DataFrame={'mode': 'r'},
-        upper_constrast_limit={"min": -100000, "max": 100000}
+        lower_contrast_limit={"min": -1000000, "max": 1000000},
+        upper_contrast_limit={"min": -1000000, "max": 1000000},
+        feature = {"choices": [""]},
+        label_column = {"choices": [""]}, widget_init=_init,
         )
 def feature_vis(label_layer: "napari.layers.Labels",
                 DataFrame: pathlib.Path,
-                feature = 'feature1',
-                label_column = 'label',
+                feature = '',
+                label_column = '',
                 Colormap=ColormapChoices.viridis,
-                auto_detect_contrast_limits=True,
-                lower_contrast_limit: int = 100, upper_constrast_limit: int = 900):
+                lower_contrast_limit: int = 100, upper_contrast_limit: int = 900):
     # TODO: handle the colormap choice
-    site_df = pd.read_csv(DataFrame)
+    site_df = get_df(DataFrame)
     site_df.loc[:, 'label'] = site_df[str(label_column)].astype(int)
     # Check that there is one unique label for every entry in the dataframe
     # => It's a site dataframe, not one containing many different sites
+    # TODO: How to feedback this issue to the user?
     assert len(site_df['label'].unique()) == len(site_df), 'A feature dataframe with non-unique labels was provided. The visualize_feature_on_label_layer function is not designed for this.'
     # Rescale feature between 0 & 1 to make a colormap
-    # If a threshold tuple is provided, the first value is used as lower and the second value as upper boundary
-    if auto_detect_contrast_limits:
-        quantiles=(0.01, 0.99)
-        lower_contrast_limit = site_df[feature].quantile(quantiles[0])
-        upper_constrast_limit = site_df[feature].quantile(quantiles[1])
-        
     site_df['feature_scaled'] = (
-        (site_df[feature] - lower_contrast_limit) / (upper_constrast_limit - lower_contrast_limit)
+        (site_df[feature] - lower_contrast_limit) / (upper_contrast_limit - lower_contrast_limit)
     )
     # Cap the measurement between 0 & 1
     site_df.loc[site_df['feature_scaled'] < 0, 'feature_scaled'] = 0
@@ -63,7 +102,10 @@ def feature_vis(label_layer: "napari.layers.Labels",
 
     colormap = dict(zip(site_df['label'], colors))
     label_layer.color = colormap
+    # TODO: Wait for fix for https://github.com/napari/napari/issues/2477
+    # Once this is in release, add this to the plugin
     #label_layer.properties = label_properties
+
 
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
