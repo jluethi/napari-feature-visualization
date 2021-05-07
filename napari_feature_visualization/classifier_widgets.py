b381778b27ea667a9672763cf43c842215dc144a
jluethi <joel.luethi@uzh.ch>
2021-05-07 23:13:38 +0200

    Major refactor of classifier plugin: Create the ClassifierWidget class

--- napari_feature_visualization/classifier_widgets.py
+++ napari_feature_visualization/classifier_widgets.py
@@ -53,7 +53,6 @@ def initialize_classifier(viewer: Viewer,
                       feature_selection='',
                       additional_features='',
                       label_column=''):
-    # TODO: Major refactor: Create a class ClassifierWidget. Instead of passing all the info, have it in there. Plus, have good init function
     # TODO: Make feature selection a widget that allows multiple features to be selected, not just one
     # Something like this in QListWidget: https://stackoverflow.com/questions/4008649/qlistwidget-and-multiple-selection
     # See issue here: https://github.com/napari/magicgui/issues/229
@@ -69,24 +68,8 @@ def initialize_classifier(viewer: Viewer,
     index_columns=('path', label_column)
     site_df = site_df.set_index(list(index_columns))
     clf = Classifier(name=classifier_name, features=site_df, training_features=training_features, index_columns=index_columns)
-    clf.save()
 
-    # Create a selection & prediction layer
-    # TODO: Handle state when those layers were already created. Replace them otherwise?
-    # https://napari.org/guides/stable/magicgui.html#updating-an-existing-layer
-    prediction_layer = viewer.add_labels(label_layer.data, name='prediction', opacity=1.0, scale=label_layer.scale)
-    selection_layer = viewer.add_labels(label_layer.data, name='selection', opacity=1.0, scale=label_layer.scale)
-    colordict = create_label_colormap(selection_layer, clf.train_data, 'train', DataFrame)
-    create_label_colormap(prediction_layer, clf.predict_data, 'predict', DataFrame)
-    viewer.layers.selection.clear()
-    viewer.layers.selection.add(label_layer)
-
-    widget = selector_widget(clf, label_layer, DataFrame, selection_layer, prediction_layer, viewer, colordict)
-
-    # TODO: Add a warning if a classifier with this name already exists => shall it be overwritten? => Confirmation box
-
-    # add widget to napari
-    viewer.window.add_dock_widget(widget, area='right', name=classifier_name)
+    ClassifierWidget(clf, label_layer, DataFrame, viewer)
 
 
 @magic_factory(
@@ -103,7 +86,7 @@ def load_classifier(viewer: Viewer,
     # TODO: Add ability to see currently selected features
     # TODO: Ensure classifier path ends in .clf and DataFrame path ends in .csv
     # TODO: Can I take a guess for the dataframe path & name based on the filename of the site? Can I get the filename of the current site somehow? => probably not?
-    classifier_name = classifier_path.stem
+    #classifier_name = classifier_path.stem
 
     with open(classifier_path, 'rb') as f:
         clf = pickle.loads(f.read())
@@ -118,102 +101,113 @@ def load_classifier(viewer: Viewer,
     site_df = site_df.set_index(list(index_columns))
 
     clf.add_data(site_df, training_features=training_features, index_columns=index_columns)
-    clf.save()
 
-    # Create a selection & prediction layer
-    # TODO: Handle state when those layers were already created. Replace them otherwise?
-    # https://napari.org/guides/stable/magicgui.html#updating-an-existing-layer
-    prediction_layer = viewer.add_labels(label_layer.data, name='prediction', opacity=1.0, scale=label_layer.scale)
-    selection_layer = viewer.add_labels(label_layer.data, name='selection', opacity=1.0, scale=label_layer.scale)
-    colordict = create_label_colormap(selection_layer, clf.train_data, 'train', DataFrame)
-    create_label_colormap(prediction_layer, clf.predict_data, 'predict', DataFrame)
-    viewer.layers.selection.clear()
-    viewer.layers.selection.add(label_layer)
-
-    widget = selector_widget(clf, label_layer, DataFrame, selection_layer, prediction_layer, viewer, colordict)
-
-    # add widget to napari
-    viewer.window.add_dock_widget(widget, area='right', name=classifier_name)
+    ClassifierWidget(clf, label_layer, DataFrame, viewer)
 
 
-def selector_widget(clf, label_layer, DataFrame, selection_layer, prediction_layer, viewer, colordict):
-    # TODO: Define a minimum number of selections. Below that, show a warning (training-test split can be very weird otherwise, e.g all 1 class)
-    # TODO: Generalize this. Instead of 0, 1, 2: Arbitrary class numbers. Ability to add classes & name them?
-    choices = ['Deselect', 'Class 1', 'Class 2', 'Class 3', 'Class 4']
-    selector = widgets.RadioButtons(choices=choices, label='Selection Class:')
-    save_button = widgets.PushButton(value=True, text='Save Classifier')
-    run_button = widgets.PushButton(value=True, text='Run Classifier')
-    container = widgets.Container(widgets=[selector, save_button, run_button])
-    # TODO: Add text field & button to save classifier output to disk
+class ClassifierWidget:
+    def __init__(self, clf, label_layer, DataFrame, viewer):
+        self.clf = clf
+        # TODO: Add a warning if a classifier with this name already exists => shall it be overwritten? => Confirmation box
+        self.clf.save()
+        self.label_layer = label_layer
+        self.DataFrame = DataFrame
+        self.viewer = viewer
 
-    @label_layer.mouse_drag_callbacks.append
-    def toggle_label(label_layer, event):
-        selection_layer.visible=True
-        # Need to scale position that event.position returns by the label_layer scale.
-        # If scale is (1, 1, 1), nothing changes
-        # If scale is anything else, this makes the click still match the correct label
-        scaled_position = tuple(pos / scale for pos, scale in zip(event.position, label_layer.scale))
-        label = label_layer.get_value(scaled_position)
-        #label = label_layer.get_value(event.position)
-        # Check if background or foreground was clicked. If background was clicked, do nothing (background can't be assigned a class)
-        if label == 0:
-            pass
-        else:
-            # Check if the label exists in the current dataframe. Otherwise, do nothing
-            if (DataFrame, label) in clf.train_data.index:
-                # Assign name of class
-                #clf.train_data.loc[(DataFrame, label)] = selector.value
-                # Assign a numeric value to make it easier (colormap currently only supports this mode)
-                clf.train_data.loc[(DataFrame, label)] = choices.index(selector.value)
-                update_label_colormap(selection_layer, label, choices.index(selector.value), colordict)
-            else:
-                # TODO: Give feedback to the user that there is no data for a specific label object in the dataframe provided?
+        # Parameters for the colormaps
+        # TODO: Generalize number of classes & colormap?
+        self.nb_classes = 4
+        self.cmap = ListedColormap([(0.0, 0.0, 0.0, 0.0), (1.0, 0.0, 0.0, 1.0), (0.0, 1.0, 0.0, 1.0), (0.0, 0.0, 1.0, 1.0), (1.0, 0.0, 1.0, 1.0)])
+
+        # Create a selection & prediction layer
+        # TODO: Handle state when those layers were already created. Replace them otherwise?
+        # https://napari.org/guides/stable/magicgui.html#updating-an-existing-layer
+        self.prediction_layer = viewer.add_labels(label_layer.data, name='prediction', opacity=1.0, scale=label_layer.scale)
+        self.selection_layer = viewer.add_labels(label_layer.data, name='selection', opacity=1.0, scale=label_layer.scale)
+        self.colordict = self.create_label_colormap(self.selection_layer, clf.train_data, 'train')
+        self.create_label_colormap(self.prediction_layer, clf.predict_data, 'predict')
+        self.viewer.layers.selection.clear()
+        self.viewer.layers.selection.add(label_layer)
+
+        widget = self.create_selector_widget(self.label_layer)
+
+        # add widget to napari
+        viewer.window.add_dock_widget(widget, area='right', name=clf.name)
+
+    def create_selector_widget(self, label_layer):
+        # TODO: Define a minimum number of selections. Below that, show a warning (training-test split can be very weird otherwise, e.g all 1 class)
+        # TODO: Generalize this. Instead of 0, 1, 2: Arbitrary class numbers. Ability to add classes & name them?
+        choices = ['Deselect', 'Class 1', 'Class 2', 'Class 3', 'Class 4']
+        # TODO: Make default choice Class 1
+        selector = widgets.RadioButtons(choices=choices, label='Selection Class:')
+        save_button = widgets.PushButton(value=True, text='Save Classifier')
+        run_button = widgets.PushButton(value=True, text='Run Classifier')
+        container = widgets.Container(widgets=[selector, save_button, run_button])
+        # TODO: Add text field & button to save classifier output to disk
+
+        @label_layer.mouse_drag_callbacks.append
+        def toggle_label(obj, event):
+            self.selection_layer.visible=True
+            # Need to scale position that event.position returns by the label_layer scale.
+            # If scale is (1, 1, 1), nothing changes
+            # If scale is anything else, this makes the click still match the correct label
+            scaled_position = tuple(pos / scale for pos, scale in zip(event.position, label_layer.scale))
+            label = label_layer.get_value(scaled_position)
+            #label = label_layer.get_value(event.position)
+            # Check if background or foreground was clicked. If background was clicked, do nothing (background can't be assigned a class)
+            if label == 0:
                 pass
+            else:
+                # Check if the label exists in the current dataframe. Otherwise, do nothing
+                if (self.DataFrame, label) in self.clf.train_data.index:
+                    # Assign name of class
+                    #self.clf.train_data.loc[(DataFrame, label)] = selector.value
+                    # Assign a numeric value to make it easier (colormap currently only supports this mode)
+                    self.clf.train_data.loc[(self.DataFrame, label)] = choices.index(selector.value)
+                    self.update_label_colormap(self.selection_layer, label, choices.index(selector.value))
+                else:
+                    # TODO: Give feedback to the user that there is no data for a specific label object in the dataframe provided?
+                    pass
 
-    @selector.changed.connect
-    def change_choice(choice):
-        selection_layer.visible=True
-        #prediction_layer.visible=False
-        viewer.layers.selection.clear()
-        viewer.layers.selection.add(label_layer)
+        @selector.changed.connect
+        def change_choice(choice):
+            self.selection_layer.visible=True
+            self.viewer.layers.selection.clear()
+            self.viewer.layers.selection.add(self.label_layer)
 
-    @save_button.changed.connect
-    def save_classifier(event):
-        print('Saving classifier')
-        clf.save()
+        @save_button.changed.connect
+        def save_classifier(event):
+            print('Saving classifier')
+            self.clf.save()
 
-    @run_button.changed.connect
-    def run_classifier(event):
-        print('Running classifier')
-        clf.train()
-        create_label_colormap(prediction_layer, clf.predict_data, 'predict', DataFrame)
-        clf.save()
-        selection_layer.visible=False
-        prediction_layer.visible=True
-        # TODO: Report classifier performance to the user?
+        @run_button.changed.connect
+        def run_classifier(event):
+            # TODO: Add Run mode? Fuzzy, Cross-validated, train/test split
+            print('Running classifier')
+            self.clf.train()
+            self.create_label_colormap(self.prediction_layer, self.clf.predict_data, 'predict')
+            self.clf.save()
+            self.selection_layer.visible=False
+            self.prediction_layer.visible=True
+            # TODO: Report classifier performance to the user?
 
-    return container
-
-
-def update_label_colormap(label_layer, label, new_class, colordict):
-    # TODO: Profile this function => can I speed it up?
-    # One idea for speed-up: Change to 2 functions. 1 for initial calculation, returns colordict.
-    # New one for just updating a single label
-    nb_classes = 4
-    cmap = ListedColormap([(0.0, 0.0, 0.0, 0.0), (1.0, 0.0, 0.0, 1.0), (0.0, 1.0, 0.0, 1.0), (0.0, 0.0, 1.0, 1.0), (1.0, 0.0, 1.0, 1.0)])
-
-    colordict[label] = cmap(new_class/nb_classes)
-    label_layer.color = colordict
+        return container
 
 
-def create_label_colormap(label_layer, df, feature, DataFrame):
-    # TODO: Profile this function => can I speed it up?
-    nb_classes = 4
-    cmap = ListedColormap([(0.0, 0.0, 0.0, 0.0), (1.0, 0.0, 0.0, 1.0), (0.0, 1.0, 0.0, 1.0), (0.0, 0.0, 1.0, 1.0), (1.0, 0.0, 1.0, 1.0)])
-    color_dict = {}
-    site_df = df[df.index.isin([DataFrame], level=0)]
-    site_df.index = site_df.index.droplevel()
-    colors = cmap(site_df[feature]/nb_classes)
-    colordict = dict(zip(site_df.index, colors))
-    label_layer.color = colordict
-    return colordict
+    def update_label_colormap(self, label_layer, label, new_class):
+        # TODO: This is still kinda laggy on large dataset. Profile this function => can I speed it up?
+        # Is there a way to not send a whole new colormap, but just change the colormap in one place?
+        # Check if the label_layer has a colormap property that could be modified
+        # Check here: viewer.layers['labels'].colormap
+        # If I can figure out how to change just the colormap, maybe that is faster than sending a whole colordict
+        self.colordict[label] = self.cmap(new_class/self.nb_classes)
+        label_layer.color = self.colordict
+
+
+    def create_label_colormap(self, label_layer, df, feature):
+        site_df = df[df.index.isin([self.DataFrame], level=0)]
+        site_df.index = site_df.index.droplevel()
+        colors = self.cmap(site_df[feature]/self.nb_classes)
+        colordict = dict(zip(site_df.index, colors))
+        label_layer.color = colordict
+        return colordict
